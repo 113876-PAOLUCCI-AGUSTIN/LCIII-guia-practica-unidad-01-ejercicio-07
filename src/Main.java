688a0f07aa77e2dc6a3c87ccbcfe01959ab9e63a
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-21 16:59:38 -0300

    Enunciado cargado.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+/*
+* 7. Una empresa de alquiler de autos cobra $300 por día si no se superan los 200 km
+* de uso diario. Por cada km extra hasta los 1000 km cobra $5 adicionales, y a partir
+* de los 1000 cobra $10 adicionales. Hacer un programa que solicite la cantidad de km
+* realizados por un cliente e indique el importe que se le debe cobrar.
+*/
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
     }
 }
